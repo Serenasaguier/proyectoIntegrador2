23090afc01d295ea52fc7009576ae826966a9cd4
Miguel Guerrero <miguelguerrero@192.168.1.5>
2023-06-11 21:00:52 -0300

    comment/formcoment

--- mi-proyecto/src/components/FormComment.js
+++ mi-proyecto/src/components/FormComment.js
@@ -1,9 +1,75 @@
+import { Text, View , TextInput, TouchableOpacity, StyleSheet} from 'react-native'
 import React, { Component } from 'react'
+import { db , auth} from '../firebase/config'
+import firebase from 'firebase'
 
 export default class FormComment extends Component {
+    constructor(props){
+        super(props)
+        this.state= {
+            comentario: ''
+        }
+    }
+
+    crearComment(comentario){
+        db.collection('posts')
+        .doc(this.props.idPost)
+        .update({
+            comments: firebase.firestore.FieldValue.arrayUnion({
+                owner: auth.currentUser.email,
+                createdAt: Date.now(),
+                comentario: comentario
+            })
+        })
+    }
+
   render() {
     return (
-      <div>FormComment</div>
+      <View style={styles.container}>
+        <TextInput
+        keyboardType='default'
+        style={styles.input}
+        onChangeText={text => this.setState({comentario: text})}
+        value= {this.state.comentario}
+        placeholder='Envia tu comentario'
+        />
+
+        <TouchableOpacity 
+        onPress={()=> this.crearComment(this.state.comentario)}
+        >
+            <Text style={styles.btnSend}>Enviar comentario</Text>
+        </TouchableOpacity>
+      </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+    input:{
+        color: 'rgb(0,0,0)',
+        borderStyle: 'solid',
+        borderWidth: 1,
+        borderColor: 'rgb(0,0,0)',
+        backgroundColor: 'rgb(255,255,255)',
+        padding: 10,
+        margin: 10
+    },
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+        color: 'rgb(255,255,255)',
+        padding: 15,
+        justifyContent: 'center',
+        textAlign: 'center',
+    },
+    btnSend:{
+        borderStyle: 'solid',
+        borderWidth: 1,
+        backgroundColor: 'rgb(0,0,0)',
+        margin: 10,
+        padding: 10,
+        textAlign: 'center',
+        color: 'white',
+        borderRadius: 8,
+    }
+})
--- mi-proyecto/src/screens/Comment.js
+++ mi-proyecto/src/screens/Comment.js
@@ -1,10 +1,84 @@
+import { Text, View, FlatList, StyleSheet, TouchableOpacity } from 'react-native'
 import React, { Component } from 'react'
-import FormComment from '../components/FormComment'
+import FormComments from '../components/FormComments'
+import { db } from '../firebase/config'
+import { AntDesign } from '@expo/vector-icons';
 
-export default class Comment extends Component {
+class Comment extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: {}
+    }
+  }
+
+  componentDidMount() {
+    db.collection('posts')
+      .doc(this.props.route.params.id)
+      .onSnapshot(doc => {
+        this.setState({
+          data: doc.data()
+        }, () => console.log(this.state.data))
+      })
+  }
+
   render() {
+
     return (
-      <FormComment navigation={this.props.navigation} />
+      <View style={styles.container}>
+        <TouchableOpacity
+          style={styles.flecha}
+          onPress={() => this.props.navigation.navigate('Home')}
+        >
+          <Text>
+            <AntDesign name='arrowleft' size={24} color='black' />
+            HOME
+          </Text>
+        </TouchableOpacity>
+        
+
+        <FlatList
+          style={styles.comentarios}
+          data={this.state.data.comments}
+          keyExtractor={item => item.createdAt.toString}
+          renderItem={({ item }) => <Text>{item.comentario}</Text>}
+        />
+        <FormComment idPost={this.props.route.params.id} />
+
+
+      </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  comentarios: {
+    padding: 10,
+    flexDirection: 'column',
+    flex: 10,
+  },
+  boton: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+    color: 'white'
+  },
+  btn: {
+    marginTop: 32,
+    backgroundColor: '#54d0e0',
+    padding: 10,
+    borderRadius: 20
+  },
+  flecha: {
+    alignItems: 'start'
+  },    
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    color: 'rgb(255,255,255)',
+    padding: 15,
+    justifyContent: 'center',
+    textAlign: 'center',
+}
+})
+
+export default Comment
